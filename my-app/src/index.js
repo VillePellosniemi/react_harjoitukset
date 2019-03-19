d09118b18b0ba26637f38421ef9d6459387f123f
VillePellosniemi <34025719+VillePellosniemi@users.noreply.github.com>
2019-03-19 12:45:11 +0200

    update

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Table from './Table';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Table />, document.getElementById('root'));
 
